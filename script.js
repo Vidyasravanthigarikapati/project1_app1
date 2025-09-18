41bdfb2f548d90e56a0158162c0a67aeefba16ce
vidya <159389388+Vidyasravanthigarikapati@users.noreply.github.com>
2025-09-18 08:45:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Initialize the map
 var map = L.map('map').setView([41.8781, -87.6298], 11);
 
-// Add base map tiles
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Add base map tiles (dark)
+L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
@@ -15,9 +15,11 @@ var ratIcon = L.icon({
 // Chicago neighborhoods (polygons)
 $.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/chicago.geojson", function(data) {
   L.geoJson(data, {
-    style: { color: "blue", weight: 2, fillOpacity: 0.1 },
+    style: { color: "purple", weight: 2, fillOpacity: 0.2 },
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Neighborhood: " + (feature.properties.community || "N/A"));
+      layer.bindPopup(
+        "<b>Neighborhood:</b> " + (feature.properties.community || "N/A")
+      );
     }
   }).addTo(map);
 });
@@ -27,17 +29,43 @@ $.getJSON("https://data.cityofchicago.org/resource/97t6-zrhs.geojson?$limit=1000
   L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
       return L.marker(latlng, { icon: ratIcon })
-        .bindPopup("Rodent Complaint ID: " + (feature.properties.service_request_number || "Unknown"));
+        .bindPopup(
+          "<b>Rodent Complaint</b><br>" +
+          "ID: " + (feature.properties.service_request_number || "Unknown")
+        );
     }
   }).addTo(map);
 });
 
-// Load rodents.geojson (rodent sightings)
+// extra geojsonrodents , Load rodents.geojson (rodent sightings)
 $.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/rodents.geojson", function(data) {
   L.geoJson(data, {
     pointToLayer: function(feature, latlng) {
       return L.marker(latlng, { icon: ratIcon })
-        .bindPopup("Rodent Report ID: " + (feature.properties.SR_NUMBER || "Unknown"));
+        .bindPopup(
+          "<b>Rodent Report</b><br>" +
+          "Report ID: " + (feature.properties.SR_NUMBER || "Unknown")
+        );
     }
   }).addTo(map);
 });
+
+// --- Add Legend ---
+var legend = L.control({ position: "bottomright" });
+legend.onAdd = function(map) {
+  var div = L.DomUtil.create("div", "info legend");
+  div.innerHTML += "<h4>Legend</h4>";
+  div.innerHTML += '<i style="background:purple; width:12px; height:12px; display:inline-block; margin-right:5px;"></i> Neighborhoods<br>';
+  div.innerHTML += '<img src="http://maptimeboston.github.io/leaflet-intro/rat.gif" style="width:20px; margin-right:5px;"> Rodent Data<br>';
+  return div;
+};
+legend.addTo(map);
+
+// --- Add Title ---
+var title = L.control({ position: "topright" });
+title.onAdd = function(map) {
+  var div = L.DomUtil.create("div", "map-title");
+  div.innerHTML = "<h3 style='margin:0; padding:5px; background:black; color:white; border-radius:5px;'>Chicago Rodent Activity Map</h3>";
+  return div;
+};
+title.addTo(map);
